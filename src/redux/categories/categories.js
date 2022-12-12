b473ee592a0a1e34ec0561114e6df46ea4f4da47
Kweeka <mtalbi29@yahoo.com>
2022-12-12 11:17:16 +0100

    Add categories reducer

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,16 @@
+const categories = [];
+
+const CHECK_STATUS = 'bookStore/categories/CHECK_STATUS';
+
+const categoriesReducer = (state = categories, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
+
+export const checkCategoryStatus = (category) => ({ type: CHECK_STATUS, category });
+
+export default categoriesReducer;
