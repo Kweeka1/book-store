628c16c02ef7396f08366954d6e7c3fb4bb867df
Kweeka <mtalbi29@yahoo.com>
2022-12-13 11:28:23 +0100

    Update BookForm.jsx, Add ability to save form changes

--- src/Components/BookForm/BookForm.jsx
+++ src/Components/BookForm/BookForm.jsx
@@ -1,22 +1,43 @@
+import { useState } from 'react';
 import './bookForm.scss';
 
 // eslint-disable-next-line react/prop-types
-const BookForm = ({ categories, handleFormSubmit }) => (
-  <div className="book-form-section">
-    <h1>Add a new Book</h1>
-    <form onSubmit={handleFormSubmit} className="book-form">
-      <input name="book-title" type="text" placeholder="Book title" />
-      <select name="book-category" id="categories">
-        {
-          // eslint-disable-next-line react/prop-types
-          categories.map((category) => (
-            <option key={category} value={category}>{category}</option>
-          ))
-        }
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+const BookForm = ({ authors, handleFormSubmit }) => {
+  const [book, setBook] = useState({
+    title: '',
+    author: authors[0],
+  });
+
+  return (
+    <div className="book-form-section">
+      <h1>Add a new Book</h1>
+      <form
+        onReset={() => setBook({ title: '', author: authors[0] })}
+        onSubmit={(ev) => handleFormSubmit(ev, book)}
+        className="book-form"
+      >
+        <input
+          onInput={(ev) => setBook((prev) => ({ ...prev, title: ev.target.value }))}
+          name="book-title"
+          type="text"
+          placeholder="Book title"
+        />
+        <select
+          onChange={(ev) => setBook((prev) => ({ ...prev, author: ev.target.value }))}
+          name="book-auhor"
+          id="authors"
+        >
+          {
+            // eslint-disable-next-line react/prop-types
+            authors.map((auhor) => (
+              <option key={auhor} value={auhor}>{auhor}</option>
+            ))
+          }
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
