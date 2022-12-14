d408e6f20bf95148c29ae67e4ecf1972f5a6641f
Kweeka <mtalbi29@yahoo.com>
2022-12-14 14:27:01 +0100

    Add ability to load books on app load

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.scss';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Books from './Pages/Books/Books';
 import Header from './Components/Header/Header';
 import Categories from './Pages/Categories/Categories';
+import { loadBooksRequest } from './redux/books/books';
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  dispatch(loadBooksRequest());
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
